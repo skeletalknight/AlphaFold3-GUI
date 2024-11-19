dc0c41f99a8820837ba26dffe86acea73c811e51
Han Wang <marspenman@gmail.com>
2024-11-19 14:17:36 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
@@ -16,8 +10,6 @@ author = 'Hanzi'
 release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'myst_parser',
@@ -33,8 +25,6 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 # The theme to use for HTML and HTML Help pages.
 html_theme = 'furo'
 html_theme_options = {
